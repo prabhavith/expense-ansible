de8095f1531e049554227b55fa48094cde71df1f
prabhavith <you@example.com>
2024-01-05 00:04:21 +0530

    pushing!

--- IAC/modules/app/variables.tf
+++ IAC/modules/app/variables.tf
@@ -1,10 +1,12 @@
 variable "env" {}
 variable "backend" {
-  component = "backend"
-  min_count = 1
-  max_count = 2
-  app_port = 8080
-  instance_type = "t2.small"
+  default = {
+    component = "backend"
+    min_count = 1
+    max_count = 2
+    app_port = 8080
+    instance_type = "t2.small"
+  }
 }
 variable "vpc_id" {}
 variable "vpc_cidr" {}
