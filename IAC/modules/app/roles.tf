3fd6e14194aad75d8ea052643f117b8de7105449
prabhavith <you@example.com>
2024-01-15 22:32:23 +0530

    pushing!

--- IAC/modules/app/roles.tf
+++ IAC/modules/app/roles.tf
@@ -29,86 +29,66 @@ resource "aws_iam_role" "main" {
       ]
     })
   }
-  inline_policy {
-    name = "ec2-run"
-    policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "VisualEditor1",
-          "Effect": "Allow",
-          "Action": [
-            "ec2:TerminateInstances",
-            "ec2:CreateTags",
-            "ec2:RunInstances"
-          ],
-          "Resource": "*"
-        }
-      ]
-    })
-  }
-  inline_policy {
-    name = "prometheus-alertmanager-publish-alert-sns"
-    policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "VisualEditor2",
-          "Effect": "Allow",
-          "Action": "sns:Publish",
-          "Resource": "arn:aws:sns:us-east-1:180933357369:Prometheus-alertmanager"
-        }
-      ]
-    })
-
-  }
-  inline_policy {
-    name = "s3-getobject"
-    policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "VisualEditor3",
-          "Effect": "Allow",
-          "Action": [
-            "s3:GetObject",
-            "s3:ListBucket"
-          ],
-          "Resource": [
-            "arn:aws:s3:::prometheusalerts",
-            "arn:aws:s3:::prometheusalerts/*",
-            "arn:aws:s3:::terraterraform-tfstate.backup",
-            "arn:aws:s3:::terraform-tfstate.backup/*"
-          ]
-        }
-      ]
-    })
-  }
-  inline_policy {
-    name = "s3-put-object"
-    policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "VisualEditor4",
-          "Effect": "Allow",
-          "Action": [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:GetObjectTagging",
-            "s3:ListBucket",
-            "s3:GetBucketOwnershipControls"
-          ],
-          "Resource": [
-            "arn:aws:s3:::prometheusalerts/*",
-            "arn:aws:s3:::terraform-tfstate.backup/*",
-            "arn:aws:s3:::prometheusalerts",
-            "arn:aws:s3:::terraform-tfstate.backup"
-          ]
-        }
-      ]
-    })
-  }
+#  inline_policy {
+#    name = "ec2-run"
+#    policy = jsonencode({
+#      "Version": "2012-10-17",
+#      "Statement": [
+#        {
+#          "Sid": "VisualEditor1",
+#          "Effect": "Allow",
+#          "Action": [
+#            "ec2:TerminateInstances",
+#            "ec2:CreateTags",
+#            "ec2:RunInstances"
+#          ],
+#          "Resource": "*"
+#        }
+#      ]
+#    })
+#  }
+#  inline_policy {
+#    name = "prometheus-alertmanager-publish-alert-sns"
+#    policy = jsonencode({
+#      "Version": "2012-10-17",
+#      "Statement": [
+#        {
+#          "Sid": "VisualEditor2",
+#          "Effect": "Allow",
+#          "Action": "sns:Publish",
+#          "Resource": "arn:aws:sns:us-east-1:180933357369:Prometheus-alertmanager"
+#        }
+#      ]
+#    })
+#
+#  }
+#  inline_policy {
+#    name = "s3-get-put-object"
+#    policy = jsonencode({
+#      "Version": "2012-10-17",
+#      "Statement": [
+#        {
+#          "Sid": "VisualEditor3",
+#          "Effect": "Allow",
+#          "Action": [
+#            "s3:GetObject",
+#            "s3:ListBucket",
+#            "s3:PutObject",
+#            "s3:GetObject",
+#            "s3:GetObjectTagging",
+#            "s3:ListBucket",
+#            "s3:GetBucketOwnershipControls"
+#          ],
+#          "Resource": [
+#            "arn:aws:s3:::prometheusalerts",
+#            "arn:aws:s3:::prometheusalerts/*",
+#            "arn:aws:s3:::terraterraform-tfstate.backup",
+#            "arn:aws:s3:::terraform-tfstate.backup/*"
+#          ]
+#        }
+#      ]
+#    })
+#  }
 }
 
 resource "aws_iam_instance_profile" "main" {
