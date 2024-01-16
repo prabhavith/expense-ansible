ca76773a97422c193bf2ad96d6d98c99b840feeb
prabhavith <you@example.com>
2024-01-16 15:21:25 +0530

    pushing!

--- IAC/modules/app/userdata.sh
+++ IAC/modules/app/userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-dnf install ansible -y
-pip3 install boto3 botocore
-ansible-pull -i localhost, -U https://github.com/prabhavith/expense-ansible.git expense.yml -e tier=${component}
+touch /home/centos/userdata.log
+dnf install ansible -y | tee -a /home/centos/userdata.log
+pip3 install boto3 botocore | tee -a /home/centos/userdata.log
+ansible-pull -i localhost, -U https://github.com/prabhavith/expense-ansible.git expense.yml -e tier=${component} | tee -a /home/centos/userdata.log
